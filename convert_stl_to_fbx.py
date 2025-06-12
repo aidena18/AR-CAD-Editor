9d6172d58b07278032c79befb7e698911a6c23c8
aidena18 <aiden.annis06@gmail.com>
2025-06-12 15:36:24 -0400

    Update convert_stl_to_fbx.py

--- convert_stl_to_fbx.py
+++ convert_stl_to_fbx.py
@@ -11,12 +11,6 @@ fbx_path = argv[1]
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete(use_global=False)
 
-# Try to import STL using the built-in operator name (manually registered in some versions)
-try:
-    bpy.ops.wm.stl_import(filepath=stl_path)
-except:
-    print("❌ Failed to import STL using 'bpy.ops.wm.stl_import'")
-    raise
 
 # Export to FBX
 bpy.ops.export_scene.fbx(filepath=fbx_path)
